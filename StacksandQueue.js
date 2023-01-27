084d222cc5883c89cc0f4e2c378df4288aed4b6d
jcochran206 <jonathan.o.cochran@gmail.com>
2023-01-26 23:39:12 -0800

    add stacks and queues

---
+++ StacksandQueue.js
@@ -0,0 +1,14 @@
+class Node {
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.top = null;
+        this.buttom = null;
+        this.length = 0;
+    }
+}
