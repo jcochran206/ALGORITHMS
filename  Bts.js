3a154c5311c2d8e01e114413a38d912c85e58776
jcochran206 <jonathan.o.cochran@gmail.com>
2023-02-03 23:59:22 -0800

    added add method in BST class

---  Bts.js
+++  Bts.js
@@ -5,3 +5,47 @@ class  Node {
         this.right = null;
     }
 }
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    //methods
+    add(data){
+        const newNode = new Node(data);
+        //look if root node
+        if(this.root === null) {
+            this.root = newNode;
+        } else {
+            let currentNode = this.root;
+            while(true) {
+                if(data < currentNode.data){
+                    //left
+                    if(!currentNode.left){
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left;
+                } else {
+                    //right 
+                    if(!currentNode.right){
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+    }
+
+    contains(data){
+
+    }
+}
+const tree = new BinarySearchTree();
+tree.add(9);
+tree.add(4);
+tree.add(20);
+tree.add(1);
+console.log(JSON.stringify(tree));
+
